f10a40b636a69e15bcade462a4c260db98cfd657
Nicholas Collens <njc64970@ucmo.edu>
2025-02-28 13:21:16 -0600

    add message if login values invalid

--- rpg-companion-frontend/components/CustomLogin.tsx
+++ rpg-companion-frontend/components/CustomLogin.tsx
@@ -5,20 +5,24 @@ import {Text, View, TextInput, StyleSheet, Button, Alert} from 'react-native'
 export default function CustomLogin() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
 
     const handleLogin = () => {
         console.log(username);
         console.log(password);
-        if (username == "" || password == "" ){
+        if (username == "" && password == "" ){
             console.log("Neither username or password present");
-            console.log("USERNAME: "+username);
-            console.log("PASSWORD: "+password);
+            setMessage("Please enter a valid username and password")
+
         }
-        else if (username != "" || password == "" ){
+        else if (username != "" && password == "" ){
             console.log("No password present")
+            setMessage("Please enter a valid password")
         }
-        else if (username == "" || password != "" ){
+        else if (username == "" && password != "" ){
             console.log("No username present");
+            setMessage("Please enter a valid username")
+
     }
 }
 
@@ -27,6 +31,7 @@ export default function CustomLogin() {
         <View style = {style.loginContainer}>
             <TextInput value= {username} onChangeText={setUsername} style={style.login} placeholder="Username" placeholderTextColor='darkgray'></TextInput>
             <TextInput value= {password} onChangeText={setPassword} style={style.login} placeholder="Password" placeholderTextColor='darkgray'></TextInput>
+            <Text style={style.warning}>{message}</Text>
             <Text style={style.button} onPress={handleLogin}>Login</Text>
             <Text style={style.button} onPress={ () => console.log("Register Clicked")}>Register</Text>
 
@@ -63,5 +68,9 @@ const style = StyleSheet.create ({
         marginTop:15,
         margin: "2.5%",
         fontSize: 32,
+    },
+    warning: {
+        color: "red",
+        justifyContent:"center"
     }
 })
